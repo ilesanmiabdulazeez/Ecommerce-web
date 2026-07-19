b353c54c5d2d7a5cad1494061bb8ed0adbc907be
ilesanmi abdulazeez <ilesanmiabdulazeez@gmail.com>
2026-07-19 00:04:48 -0700

    exported the products

---
+++ data/products.js
@@ -0,0 +1,75 @@
+export  const products = [{
+    id: 'df09988a-f45b-455a-909f-abb7ef3183d4',
+    image: './product-images/cooker.jpg',
+    name: 'Electric cooker',
+    priceCents: 200,
+    stars:[
+        {
+            rating: 3,
+            count: 10
+        }
+    ]
+  },{
+    id: '3444c3c2-727c-4075-9b95-8acd75df278d',
+    image: './product-images/electric-heater.jpg',
+    name: 'Electric-heater',
+    priceCents: 80,
+    stars:[{
+        rating: 4.5,
+        count: 15
+    }]
+  },{
+    id: '3d432847-2b05-4637-96a3-7b27e8c930b1',
+    image: './product-images/electric-toaster.jpg',
+    name: 'Electric portable toaster',
+    priceCents: 50,
+    stars:[{
+        rating: 2.5,
+        count: 20
+    }]
+  },{
+    id: '9a710726-656b-48c8-8011-1db6d354c482',
+    image: './product-images/family-size-fridge.jpg',
+    name: 'Family size fridge',
+    priceCents: 150,
+    stars:[{
+        rating: 3,
+        count: 12
+    }]
+  },{
+    id: 'd7779592-7ac4-4b28-afe9-efb5d933622c',
+    image: './product-images/macbook-air.jpg',
+    name: 'Macbook AIR',
+    priceCents:1000,
+    stars:[{
+        rating: 3.5,
+        count: 8
+    }]
+  },{
+    id: 'd1871676-3428-4677-be37-12a5dded5a59',
+    image: './product-images/tv.jpg',
+    name: 'Hisense LCD',
+    priceCents: 250,
+    stars:[{
+        rating: 4,
+        count: 15
+    }]
+  },{
+    id: '1a698d73-54fe-473f-823b-6f924a7e4639',
+    image: './product-images/washing-machine.jpg',
+    name: 'Portable washing maching',
+    priceCents:150,
+    stars:[{
+        rating: 5,
+        count: 50
+    }]
+  },{
+    id: 'be999b1b-5dff-4d37-86a6-1497695f8336',
+    image: './product-images/vacum-cleaner.jpg',
+    name: 'Electric Vacum tube',
+    priceCents: 300,
+    stars:[{
+        rating: 4.5,
+        count: 80
+    }]
+  }];
