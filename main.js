56c6617704ae401ca459b09daf02c5734794e1cc
ilesanmi abdulazeez <ilesanmiabdulazeez@gmail.com>
2026-07-18 20:00:48 -0700

    Added other js file path

--- main.js
+++ main.js
@@ -4,73 +4,7 @@
 // make it interactive
 
 //  * save the data
-  const products = [{
-    image: './product-images/cooker.jpg',
-    name: 'Electric cooker',
-    priceCents: 200,
-    stars:[
-        {
-            rating: 3,
-            count: 10
-        }
-    ]
-  },{
-    image: './product-images/electric-heater.jpg',
-    name: 'Electric-heater',
-    priceCents: 80,
-    stars:[{
-        rating: 4.5,
-        count: 15
-    }]
-  },{
-    image: './product-images/electric-toaster.jpg',
-    name: 'Electric portable toaster',
-    priceCents: 50,
-    stars:[{
-        rating: 2.5,
-        count: 20
-    }]
-  },{
-    image: './product-images/family-size-fridge.jpg',
-    name: 'Family size fridge',
-    priceCents: 150,
-    stars:[{
-        rating: 3,
-        count: 12
-    }]
-  },{
-    image: './product-images/macbook-air.jpg',
-    name: 'Macbook AIR',
-    priceCents:1000,
-    stars:[{
-        rating: 3.5,
-        count: 8
-    }]
-  },{
-    image: './product-images/tv.jpg',
-    name: 'Hisense LCD',
-    priceCents: 250,
-    stars:[{
-        rating: 4,
-        count: 15
-    }]
-  },{
-    image: './product-images/washing-machine.jpg',
-    name: 'Portable washing maching',
-    priceCents:150,
-    stars:[{
-        rating: 5,
-        count: 50
-    }]
-  },{
-    image: './product-images/vacum-cleaner.jpg',
-    name: 'Electric Vacum tube',
-    priceCents: 300,
-    stars:[{
-        rating: 4.5,
-        count: 80
-    }]
-  }];
+
 
     //* generate the html
     let productHtml = "";
